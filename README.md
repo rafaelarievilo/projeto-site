f9ad444dcda61dafa953afc4fe272978a52e6676
rafaelarievilo <rafalmachado1306@gmail.com>
2020-11-28 22:19:23 -0300

    Projeto site não responsivo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # projeto-site
-Praticando e estudando HTML, CSS e Javascript
+Site não responsivo, irei tentar criar um igual, mas responsivo.
+# Praticando e estudando HTML, CSS e Javascript
+não consegui implementar o javascript, mas vamos estudar pra pular essa barreira :)
---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* let intervalo = 3000;
+
+function slide1() {
+    document.getElementById('banner').src = "../img/banner.png"
+    setTimeout("slide2()", intervalo)
+}
+
+function slide2() {
+    document.getElementById('banner').src = "../img/banner2.jpg"
+    setTimeout("slide3()", intervalo)
+}
+
+function slide3() {
+    document.getElementById('banner').src = "../img/banner3.jpg"
+    setTimeout("slide1()", intervalo)
+} */
